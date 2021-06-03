105a2dee23bc0532844edffeb2b823c566a0c39c
HeartOfAutumn <50200313+HeartOfAutumn@users.noreply.github.com>
2021-06-03 21:49:56 +0700

    Upload code for multi-choice form
    Like upon

---
+++ script.js
@@ -0,0 +1,47 @@
+for(let i=1; i<=25; i++){
+    const parent =  document.getElementById('boss-1').innerHTML += `
+    <tr id="${i}">
+        <th scope="row">${i}</th>
+        <td><input type="checkbox" value="A"></td>
+        <td><input type="checkbox" value="B"></td>
+        <td><input type="checkbox" value="C"></td>
+        <td><input type="checkbox" value="C"></td>
+    </tr>
+`;
+}
+
+for(let i=26; i<=50; i++){
+    const parent =  document.getElementById('boss-2').innerHTML += `
+    <tr id="${i}">
+        <th scope="row">${i}</th>
+        <td><input type="checkbox" value="A"></td>
+        <td><input type="checkbox" value="B"></td>
+        <td><input type="checkbox" value="C"></td>
+        <td><input type="checkbox" value="C"></td>
+    </tr>
+`;
+}
+
+for(let i=51; i<=75; i++){
+    const parent =  document.getElementById('boss-3').innerHTML += `
+    <tr id="${i}">
+        <th scope="row">${i}</th>
+        <td><input type="checkbox" value="A"></td>
+        <td><input type="checkbox" value="B"></td>
+        <td><input type="checkbox" value="C"></td>
+        <td><input type="checkbox" value="C"></td>
+    </tr>
+`;
+}
+
+for(let i=76; i<=100; i++){
+    const parent =  document.getElementById('boss-4').innerHTML += `
+    <tr id="${i}">
+        <th scope="row">${i}</th>
+        <td><input type="checkbox" value="A"></td>
+        <td><input type="checkbox" value="B"></td>
+        <td><input type="checkbox" value="C"></td>
+        <td><input type="checkbox" value="C"></td>
+    </tr>
+`;
+}
